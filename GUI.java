ec3a1c4fe1e4614eafeca05a9b7c4a29b4daaa9c
rhandSJU <ryan.handley20@my.stjohns.edu>
2023-10-02 11:15:21 -0400

    Updated code

--- GUI.java
+++ GUI.java
@@ -1,87 +1,181 @@
-	
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class GUI {
-    public static void main(String[] args) {
-        // Create a JFrame
-        JFrame frame = new JFrame("Account Type Selection");
-        frame.setSize(500, 300);
+    private JFrame frame;
+    private JPanel cardPanel;
+    private CardLayout cardLayout;
+
+    private JPanel clientPanel;
+    private JPanel ownerPanel;
+
+    private JTextField userIdField;
+    private JTextField vehicleInfoField;
+    private JTextField residencyTimeField;
+
+    private JTextField clientIdField;
+    private JTextField jobDurationField;
+    private JTextField jobDeadlineField;
+
+    public GUI() {
+        frame = new JFrame("User Information App");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
 
-        //Createing a main panel to house all the other panels/components
-        JPanel mainPanel = new JPanel();
-        frame.add(mainPanel);
+        cardPanel = new JPanel();
+        cardLayout = new CardLayout();
+        cardPanel.setLayout(cardLayout);
 
-        // Creating a user selection panel
-        JPanel userSelectionPanel = new JPanel();
-        mainPanel.add(userSelectionPanel);
-        // Create a label to display instructions
-        JLabel label = new JLabel("Select your account type:");
-        userSelectionPanel.add(label);
+        clientPanel = createClientPanel();
+        ownerPanel = createOwnerPanel();
 
-        
+        cardPanel.add(clientPanel, "Client");
+        cardPanel.add(ownerPanel, "Owner");
 
-        /*Create User panels to hold user data, our buttons should navigate a user
-        to their respective dashboard.  Assume a login is necessary to access the page
-        */
-        //JPanel VO
-        JPanel VO = new JPanel();
-        JLabel VOLabel = new JLabel("Vehicle Owner Homepage");
-        VO.add(VOLabel);  //Add more functionality for Vehicle Owners within this Panel
-        
+        frame.add(cardPanel);
 
-        //JPanel JO 
+        JButton clientButton = new JButton("Client");
+        JButton ownerButton = new JButton("Owner");
 
-
-        //JPanel Admin (Garage admin)
-
-
-        //Using CardLayout to switch beteween panels
-        CardLayout CL = new CardLayout(0, 0);
-        mainPanel.setLayout(CL);
-
-        //Add components to the main panel
-        mainPanel.add(VO);
-
-
-        // Create radio buttons for account types
-        JRadioButton VO_RadioButton = new JRadioButton("Vehicle Owner");
-        JRadioButton JO_RadioButton = new JRadioButton("Job Owner");
-
-        // Create a button group for radio buttons
-        ButtonGroup buttonGroup = new ButtonGroup();
-        buttonGroup.add(VO_RadioButton);
-        buttonGroup.add(JO_RadioButton);
-
-        // Create a submit button
-        JButton submitButton = new JButton("Submit");
-
-        // Add action listener to the submit button
-        submitButton.addActionListener(new ActionListener() {
+        clientButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (VO_RadioButton.isSelected()) {
-                    CL.show(VO, "Vehicle Owner Home Page");
-                } else if (JO_RadioButton.isSelected()) {
-                    JOptionPane.showMessageDialog(userSelectionPanel, "Redirecting to Job Owner Login");
-                } else {
-                    JOptionPane.showMessageDialog(userSelectionPanel, "Please select an account type.");
-                }
+                cardLayout.show(cardPanel, "Client");
             }
         });
 
-        // Add components to the user selection panel
-        userSelectionPanel.add(VO_RadioButton);
-        userSelectionPanel.add(JO_RadioButton);
-        userSelectionPanel.add(submitButton);
+        ownerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cardLayout.show(cardPanel, "Owner");
+            }
+        });
 
-        // Center the JFrame on the screen
-        frame.setLocationRelativeTo(null);
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(clientButton);
+        buttonPanel.add(ownerButton);
 
-        // Make the frame visible
-	        frame.setVisible(true);
-	    }
-	}
+        frame.add(buttonPanel, BorderLayout.NORTH);
+
+        frame.setVisible(true);
+    }
+
+    private JPanel createClientPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(3, 2));
+
+        JLabel userIdLabel = new JLabel("Client ID:");
+        clientIdField = new JTextField();
+        JLabel jobDurationLabel = new JLabel("Job Duration:");
+        jobDurationField = new JTextField();
+        JLabel jobDeadlineLabel = new JLabel("Job Deadline:");
+        jobDeadlineField = new JTextField();
+
+        panel.add(userIdLabel);
+        panel.add(clientIdField);
+        panel.add(jobDurationLabel);
+        panel.add(jobDurationField);
+        panel.add(jobDeadlineLabel);
+        panel.add(jobDeadlineField);
+
+        JButton submitButton = new JButton("Submit");
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                saveClientInformation();
+            }
+        });
+
+        panel.add(submitButton);
+
+        return panel;
+    }
+
+    private JPanel createOwnerPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(3, 2));
+
+        JLabel userIdLabel = new JLabel("Owner ID:");
+        userIdField = new JTextField();
+        JLabel vehicleInfoLabel = new JLabel("Vehicle Info:");
+        vehicleInfoField = new JTextField();
+        JLabel residencyTimeLabel = new JLabel("Residency Time:");
+        residencyTimeField = new JTextField();
+
+        panel.add(userIdLabel);
+        panel.add(userIdField);
+        panel.add(vehicleInfoLabel);
+        panel.add(vehicleInfoField);
+        panel.add(residencyTimeLabel);
+        panel.add(residencyTimeField);
+
+        JButton submitButton = new JButton("Submit");
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                saveOwnerInformation();
+            }
+        });
+
+        panel.add(submitButton);
+
+        return panel;
+    }
+
+    private void saveClientInformation() {
+        String clientId = clientIdField.getText();
+        String jobDuration = jobDurationField.getText();
+        String jobDeadline = jobDeadlineField.getText();
+
+        // Save information to a file with a timestamp
+        saveInformationToFile("Client", clientId, jobDuration, jobDeadline);
+
+        // Clear fields
+        clientIdField.setText("");
+        jobDurationField.setText("");
+        jobDeadlineField.setText("");
+    }
+
+    private void saveOwnerInformation() {
+        String ownerId = userIdField.getText();
+        String vehicleInfo = vehicleInfoField.getText();
+        String residencyTime = residencyTimeField.getText();
+
+        // Save information to a file with a timestamp
+        saveInformationToFile("Owner", ownerId, vehicleInfo, residencyTime);
+
+        // Clear fields
+        userIdField.setText("");
+        vehicleInfoField.setText("");
+        residencyTimeField.setText("");
+    }
+
+    private void saveInformationToFile(String userType, String id, String info1, String info2) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("user_info.txt", true))) {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String timestamp = dateFormat.format(new Date());
+
+            String entry = timestamp + " - " + userType + " ID: " + id + ", Info1: " + info1 + ", Info2: " + info2;
+            writer.write(entry);
+            writer.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new GUI();
+            }
+        });
+    }
+}
