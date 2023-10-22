c7d2ef9275d251064a4aa02bf4304c9f01ce1904
rhandley1231 <107227559+rhandley1231@users.noreply.github.com>
2023-10-22 13:03:35 -0400

    Created the constructors for the User Superclass (#12)

--- User.java
+++ User.java
@@ -1,5 +1,22 @@
 public class User{
     private String userName;
     private String password;
-   
+   public User(String userName, String password){
+    this.userName = userName;
+    this.password= password;
+   }
+   //getter and setters for username and password
+   public void setUserName(String userName){
+    this.userName = userName;
+   }
+   public String getUserName(){
+    return userName;
+   }
+
+   public void setPassword(String password){
+    this.password = password;
+   }
+   public String getPassword(){
+    return password;
+   }
 }
