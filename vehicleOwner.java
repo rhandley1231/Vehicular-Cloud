1129c83ec69bd1c8b3cd9e0d87c091fb714921e6
Altin Qalliu <124823658+Ssechh@users.noreply.github.com>
2023-10-22 21:50:34 -0400

    updatedVehicleOwner

--- vehicleOwner.java
+++ vehicleOwner.java
@@ -7,20 +7,28 @@ public class vehicleOwner extends User{
         this.vehicles = vehicles;
     }
     public void parkCar(vehicle v){
+
+        // Timestamp for when the car is parked
+        Time timestamp = new Time(System.currentTimeMillis());
+
         /*Notifies the VC that an owner's certain car
          * has been parked and is ready to be rented out
          * for computations
          */ 
+        VCController.notifyCarParked(v);
+
         /*We have to get a time stamp on when the user calls 
          *This method, so we may view how long the car has been parked
          *in another method
          */
+        parkingTimestamps.put(v, timestamp);
 
     }
 
     public void leaveLot(vehicle v){
         /*Notifies the VC that an owner's certain car
         *has left the parking lot*/
+       VCController.notifyCarLeftLot(v);
     }
     public Time timeParked(vehicle v){
         return null; //Returns the time at which a particular car was parked
