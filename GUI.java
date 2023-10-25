56e60e1f3c27b4114ff6e1b821df3dce2b0f8201
a-ni-sha <76418971+a-ni-sha@users.noreply.github.com>
2023-10-24 20:30:59 -0400

    gui (#16)
    * Update GUI.java
    
    Commit to remote branch fixed some client formatting
    
    * Update GUI.java
    
    Fixed owner panel formatting
    
    * Update GUI.java
    
    Error handling to save client save owner
    
    * Update GUI.java
    
    Fixed saving info to file
    
    * Update GUI.java
    
    Added welcome panel
    
    * Update GUI.java text
    
    * Update GUI.java welcome text
    
    * Update GUI.java welcome text
    
    * Update GUI.java time label

--- GUI.java
+++ GUI.java
@@ -13,6 +13,7 @@ public class GUI {
     private JPanel cardPanel;
     private CardLayout cardLayout;
 
+    private JPanel welcomePanel; // Add the welcome panel
     private JPanel clientPanel;
     private JPanel ownerPanel;
 
@@ -25,7 +26,7 @@ public class GUI {
     private JTextField jobDeadlineField;
 
     public GUI() {
-        frame = new JFrame("User Information App");
+        frame = new JFrame("Parking Management App"); // Change the app title
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 300);
 
@@ -33,9 +34,11 @@ public class GUI {
         cardLayout = new CardLayout();
         cardPanel.setLayout(cardLayout);
 
+        welcomePanel = createWelcomePanel(); // Create the welcome panel
         clientPanel = createClientPanel();
         ownerPanel = createOwnerPanel();
 
+        cardPanel.add(welcomePanel, "Welcome"); // Add the welcome panel
         cardPanel.add(clientPanel, "Client");
         cardPanel.add(ownerPanel, "Owner");
 
@@ -67,7 +70,52 @@ public class GUI {
         frame.setVisible(true);
     }
 
-    private JPanel createClientPanel() {
+    private JPanel createWelcomePanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+
+        JLabel welcomeLabel = new JLabel("Welcome to Vehicle Management App");
+        welcomeLabel.setFont(new Font("Arial", Font.BOLD, 24)); // Increase font size
+        welcomeLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+        JTextArea welcomeText = new JTextArea("The welcome page of this app serves as an entry point for Vehicle Management using static vehicular cloud computing. Users can choose to access either the \"Client\" or \"Owner\" functionality, where clients can request vehicle services and owners can provide a vehicle. This app facilitates vehicle management by connecting clients and owners through vehicular cloud computing for efficient, location-based vehicle solutions.");
+        welcomeText.setFont(new Font("Arial", Font.PLAIN, 18)); // Increase font size
+        welcomeText.setWrapStyleWord(true);
+        welcomeText.setLineWrap(true);
+        welcomeText.setOpaque(false);
+        welcomeText.setEditable(false);
+        welcomeText.setMargin(new Insets(20, 20, 20, 20)); // Add margins for centering
+
+        JScrollPane scrollPane = new JScrollPane(welcomeText);
+        scrollPane.setAlignmentX(Component.CENTER_ALIGNMENT); // Center the text
+
+        JButton startClientButton = new JButton("Client");
+        startClientButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cardLayout.show(cardPanel, "Client");
+            }
+        });
+
+        JButton startOwnerButton = new JButton("Owner");
+        startOwnerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cardLayout.show(cardPanel, "Owner");
+            }
+        });
+
+        panel.add(welcomeLabel, BorderLayout.NORTH);
+        panel.add(scrollPane, BorderLayout.CENTER);
+        panel.add(startClientButton, BorderLayout.WEST);
+        panel.add(startOwnerButton, BorderLayout.EAST);
+
+        return panel;
+    }
+
+
+    
+	private JPanel createClientPanel() {
         JPanel panel = new JPanel();
         panel.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
@@ -75,11 +123,19 @@ public class GUI {
         gbc.anchor = GridBagConstraints.WEST;
 
         JLabel userIdLabel = new JLabel("Client ID:");
-        clientIdField = new JTextField(20); // Set preferred size to 20 columns
-        JLabel jobDurationLabel = new JLabel("Job Duration:");
-        jobDurationField = new JTextField(20); // Set preferred size to 20 columns
+        clientIdField = new JTextField(20);
+
+        JLabel jobDurationLabel = new JLabel("Job Duration (hh:mm):");
+        jobDurationField = new JTextField(8);
+
         JLabel jobDeadlineLabel = new JLabel("Job Deadline:");
-        jobDeadlineField = new JTextField(20); // Set preferred size to 20 columns
+
+        // Add separate fields for month, day, year, hour, and minute
+        JTextField monthField = new JTextField(4);
+        JTextField dayField = new JTextField(4);
+        JTextField yearField = new JTextField(4);
+        JTextField hourField = new JTextField(4);
+        JTextField minuteField = new JTextField(4);
 
         gbc.gridx = 0;
         gbc.gridy = 0;
@@ -92,8 +148,6 @@ public class GUI {
 
         gbc.gridx = 0;
         gbc.gridy = 1;
-        gbc.fill = GridBagConstraints.NONE;
-        gbc.weightx = 0.0;
         panel.add(jobDurationLabel, gbc);
         gbc.gridx = 1;
         gbc.gridy = 1;
@@ -101,16 +155,52 @@ public class GUI {
         gbc.weightx = 1.0;
         panel.add(jobDurationField, gbc);
 
+        // Add explanatory text for the job duration format
+        JLabel jobDurationFormatLabel = new JLabel("Format: hh:mm (e.g., 02:30 for 2 hours and 30 minutes)");
         gbc.gridx = 0;
         gbc.gridy = 2;
-        gbc.fill = GridBagConstraints.NONE;
-        gbc.weightx = 0.0;
+        gbc.gridwidth = 2;
+        panel.add(jobDurationFormatLabel, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 3;
         panel.add(jobDeadlineLabel, gbc);
+
+        // Add labels and fields for month, day, year, hour, and minute
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        panel.add(new JLabel("Month:"), gbc);
         gbc.gridx = 1;
-        gbc.gridy = 2;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.weightx = 1.0;
-        panel.add(jobDeadlineField, gbc);
+        gbc.gridy = 4;
+        panel.add(monthField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 5;
+        panel.add(new JLabel("Day:"), gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 5;
+        panel.add(dayField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 6;
+        panel.add(new JLabel("Year:"), gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 6;
+        panel.add(yearField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 7;
+        panel.add(new JLabel("Hour:"), gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 7;
+        panel.add(hourField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 8;
+        panel.add(new JLabel("Minute:"), gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 8;
+        panel.add(minuteField, gbc);
 
         JButton submitButton = new JButton("Submit");
         submitButton.addActionListener(new ActionListener() {
@@ -121,7 +211,7 @@ public class GUI {
         });
 
         gbc.gridx = 0;
-        gbc.gridy = 3;
+        gbc.gridy = 9;
         gbc.gridwidth = 2;
         gbc.fill = GridBagConstraints.NONE;
         gbc.anchor = GridBagConstraints.CENTER;
@@ -138,11 +228,19 @@ public class GUI {
         gbc.anchor = GridBagConstraints.WEST;
 
         JLabel userIdLabel = new JLabel("Owner ID:");
-        userIdField = new JTextField(20); // Set preferred size to 20 columns
+        userIdField = new JTextField(20);
+
         JLabel vehicleInfoLabel = new JLabel("Vehicle Info:");
-        vehicleInfoField = new JTextField(20); // Set preferred size to 20 columns
+        vehicleInfoField = new JTextField(20);
+
         JLabel residencyTimeLabel = new JLabel("Residency Time:");
-        residencyTimeField = new JTextField(20); // Set preferred size to 20 columns
+
+        // Add separate fields for month, day, year, hour, and minute
+        JTextField monthField = new JTextField(4);
+        JTextField dayField = new JTextField(4);
+        JTextField yearField = new JTextField(4);
+        JTextField hourField = new JTextField(4);
+        JTextField minuteField = new JTextField(4);
 
         gbc.gridx = 0;
         gbc.gridy = 0;
@@ -165,11 +263,42 @@ public class GUI {
         gbc.gridx = 0;
         gbc.gridy = 2;
         panel.add(residencyTimeLabel, gbc);
+
+        // Add labels and fields for month, day, year, hour, and minute for residency time
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        panel.add(new JLabel("Month:"), gbc);
         gbc.gridx = 1;
-        gbc.gridy = 2;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.weightx = 1.0;
-        panel.add(residencyTimeField, gbc);
+        gbc.gridy = 3;
+        panel.add(monthField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        panel.add(new JLabel("Day:"), gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 4;
+        panel.add(dayField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 5;
+        panel.add(new JLabel("Year:"), gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 5;
+        panel.add(yearField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 6;
+        panel.add(new JLabel("Hour:"), gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 6;
+        panel.add(hourField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 7;
+        panel.add(new JLabel("Minute:"), gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 7;
+        panel.add(minuteField, gbc);
 
         JButton submitButton = new JButton("Submit");
         submitButton.addActionListener(new ActionListener() {
@@ -180,7 +309,7 @@ public class GUI {
         });
 
         gbc.gridx = 0;
-        gbc.gridy = 3;
+        gbc.gridy = 8;
         gbc.gridwidth = 2;
         gbc.fill = GridBagConstraints.NONE;
         gbc.anchor = GridBagConstraints.CENTER;
@@ -189,13 +318,61 @@ public class GUI {
         return panel;
     }
 
+    private void saveInformationToFile(String type, String id, String info1, String info2) {
+        // Get the current timestamp
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String timestamp = dateFormat.format(new Date());
+
+        // Create a filename based on the timestamp
+        String filename = type + "_" + id + "_" + timestamp + ".txt";
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            writer.write("Type: " + type);
+            writer.newLine();
+            writer.write("ID: " + id);
+            writer.newLine();
+            writer.write("Info 1: " + info1);
+            writer.newLine();
+            writer.write("Info 2: " + info2);
+            writer.newLine();
+
+            // Optionally, you can write more information as needed
+
+            JOptionPane.showMessageDialog(frame, "Information saved to file: " + filename, "Success", JOptionPane.INFORMATION_MESSAGE);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(frame, "Error saving information to file.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
     private void saveClientInformation() {
         String clientId = clientIdField.getText();
-        String jobDuration = jobDurationField.getText();
+        String jobDurationInput = jobDurationField.getText();
         String jobDeadline = jobDeadlineField.getText();
 
+        int hours = 0;
+        int minutes = 0;
+
+        // Validate and parse job duration input
+        try {
+            String[] parts = jobDurationInput.split(":");
+            if (parts.length == 2) {
+                hours = Integer.parseInt(parts[0]);
+                minutes = Integer.parseInt(parts[1]);
+            } else {
+                // Invalid format
+                JOptionPane.showMessageDialog(frame, "Invalid job duration format. Please use hh:mm (e.g., 02:30).", "Error", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+        } catch (NumberFormatException e) {
+            // Invalid number format
+            JOptionPane.showMessageDialog(frame, "Invalid job duration format. Please use hh:mm (e.g., 02:30).", "Error", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        // Handle other validations as needed
+
         // Save information to a file with a timestamp
-        saveInformationToFile("Client", clientId, jobDuration, jobDeadline);
+        saveInformationToFile("Client", clientId, jobDurationInput, jobDeadline);
 
         // Clear fields
         clientIdField.setText("");
@@ -206,28 +383,37 @@ public class GUI {
     private void saveOwnerInformation() {
         String ownerId = userIdField.getText();
         String vehicleInfo = vehicleInfoField.getText();
-        String residencyTime = residencyTimeField.getText();
+        String residencyTimeInput = residencyTimeField.getText();
+
+        int hours = 0;
+        int minutes = 0;
+
+        // Validate and parse residency time input
+        try {
+            String[] parts = residencyTimeInput.split(":");
+            if (parts.length == 2) {
+                hours = Integer.parseInt(parts[0]);
+                minutes = Integer.parseInt(parts[1]);
+            } else {
+                // Invalid format
+                JOptionPane.showMessageDialog(frame, "Invalid residency time format. Please use hh:mm (e.g., 02:30).", "Error", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+        } catch (NumberFormatException e) {
+            // Invalid number format
+            JOptionPane.showMessageDialog(frame, "Invalid residency time format. Please use hh:mm (e.g., 02:30).", "Error", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        // Handle other validations as needed
 
         // Save information to a file with a timestamp
-        saveInformationToFile("Owner", ownerId, vehicleInfo, residencyTime);
+        saveInformationToFile("Owner", ownerId, vehicleInfo, residencyTimeInput);
 
         // Clear fields
         userIdField.setText("");
         vehicleInfoField.setText("");
         residencyTimeField.setText("");
-    }
-
-    private void saveInformationToFile(String userType, String id, String info1, String info2) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("user_info.txt", true))) {
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            String timestamp = dateFormat.format(new Date());
-
-            String entry = timestamp + " - " + userType + " ID: " + id + ", Info1: " + info1 + ", Info2: " + info2;
-            writer.write(entry);
-            writer.newLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     public static void main(String[] args) {
