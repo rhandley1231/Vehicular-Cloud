1300dfb3c88338ecfb9537d60d0a8c0a07b00bff
Ben Hanim <89892905+ben45123@users.noreply.github.com>
2023-10-30 23:41:49 -0400

    Created a bunch of methods across all of the classes to all them to interact with one another. (#20)
    Co-authored-by: Ben Hanim <benjamin.hanim20@stjohns.edu>

--- computationalResourceRequestor.java
+++ computationalResourceRequestor.java
@@ -1,20 +1,64 @@
+import java.text.SimpleDateFormat;
 import java.util.*;
+
 public class computationalResourceRequestor extends User {
     private ArrayList<job> jobList = new ArrayList<job>();
-    public computationalResourceRequestor(String userName, String password, ArrayList<job> jobList){
+
+    public computationalResourceRequestor(String userName, String password, ArrayList<job> jobList) {
         super(userName, password);
         this.jobList = jobList;
     }
-    //Methods for the Job Owner Class
-    public void requestJob(job j) {
-        //Adds a job to the arraylist of jobs for the owner
-    }
-    public void removeJob(job j){
-        //Removes a job from the arraylist of jobs
+
+    // Methods for the Job Owner Class
+    public void createJob(String jobTitle, String jobDescription, int jobDuration, int crr_ID, boolean assigned,
+            Date deadline, String status) {
+        job newJob = new job(jobTitle, jobDescription, jobDuration, crr_ID, assigned, deadline, status);
+        this.jobList.add(newJob);
+        VCController.currentJobList.add(newJob);
     }
 
-    public ArrayList<job> myJobs(){
-        //Allows the user to view their current jobs
-        return jobList;
+    // Adds a job to the arraylist of jobs for the owner
+    // Needs to go through the CRR's list of Jobs and Display the Necessary
+    // Information about the Job
+    public void viewCurrentJobs() { // Status: In Progress | Paused
+        for (int i = 0; i < this.jobList.size(); i++) {
+            if (this.jobList.get(i).getStatus() == "In Progress" || this.jobList.get(i).getStatus() == "Paused") {
+                viewJob(this.jobList.get(i));
+            }
+        }
     }
+
+    public void removeJob(int jobID) {
+        // Removes a job from the arraylist of jobs
+        for (int i = 0; i < this.jobList.size(); i++) {
+            if (this.jobList.get(i).getJobID() == jobID) {
+                this.jobList.remove(this.jobList.get(i));
+            }
+        }
+        for (int i = 0; i < VCController.currentJobList.size(); i++) {
+            if (VCController.currentJobList.get(i).getJobID() == jobID) {
+                VCController.currentJobList.get(i).setStatus("Cancelled");
+                VCController.pastJobList.add(VCController.currentJobList.get(i));
+                VCController.currentJobList.remove(VCController.currentJobList.get(i));
+            }
+        }
+    }
+
+    public void viewPastJobs() { // Status: Completed | Cancelled
+        for (int i = 0; i < this.jobList.size(); i++) {
+            if (this.jobList.get(i).getStatus() == "Completed" || this.jobList.get(i).getStatus() == "Cancelled") {
+                viewJob(this.jobList.get(i));
+            }
+        }
+    }
+
+    SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm");
+
+    public void viewJob(job a) {
+        System.out.println("Job Title: " + a.getJobTitle() + ", Job Description: " + a.getJobDescription());
+        System.out.println("Job Duration: " + a.getJobDuration() + " hours, Job ID: " + a.getJobID()
+                + ", Resource Requestor ID: " + a.getCRR_ID());
+        System.out.println("Status: " + a.getStatus() + ", Deadline: " + dateFormat.format(a.getDeadline()));
+    }
+
 }
