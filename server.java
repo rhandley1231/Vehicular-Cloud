176b45319077823208173291c2583385ee89521a
Ben Hanim <benjamin.hanim20@stjohns.edu>
2023-10-31 14:50:30 -0400

    Created a bunch of additional methods, including the checkpoint, and assign jobs to vehicles, in addition to implenting the server class

--- server.java
+++ server.java
@@ -1,3 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class server {
-    
+    public static List<job> pastJobList = new ArrayList<job>();
+    public static List<vehicle> pastParkedVehicles = new ArrayList<vehicle>();
+
+    public void viewPastJobs() {
+        for (int i = 0; i < pastJobList.size(); i++) {
+            VCController.viewJob(pastJobList.get(i));
+        }
+    }
+
+    public void viewPastParkedVehicles() {
+        for (int i = 0; i < pastParkedVehicles.size(); i++) {
+            VCController.viewVehicle(pastParkedVehicles.get(i));
+        }
+    }
+
 }
