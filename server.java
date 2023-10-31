e89a588b4e661c07767648d425108ccb097c489a
rhandley1231 <rhandley1231@gmail.com>
2023-10-31 11:58:08 -0400

    basic server construction

--- server.java
+++ server.java
@@ -1,3 +1,9 @@
+import java.util.*;
 public class server {
-    
+    private ArrayList<job> jobHistory = new ArrayList<job>();
+    private ArrayList<vehicle> lotHistory = new ArrayList<vehicle>();
+    public server(ArrayList<job> jH, ArrayList<vehicle> lH) {
+        this.jobHistory = jH;
+        this.lotHistory = lH;
+    }
 }
