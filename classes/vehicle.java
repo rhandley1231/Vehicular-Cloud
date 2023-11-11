6e83f000a263243538861bf306a3817320084231
CeeThorn <124415706+CeeThorn@users.noreply.github.com>
2023-11-11 11:56:48 -0500

    Update vehicle.java
    up to ate vehicle.java

--- classes/vehicle.java
+++ classes/vehicle.java
@@ -1,36 +1,18 @@
 <<<<<<< HEAD:classes/vehicle.java
 package classes;
-import java.sql.Date;
-import java.text.DateFormat;
-import java.time.LocalDateTime;
-=======
-import java.sql.Date;
-import java.text.DateFormat;
-import java.time.LocalDateTime;
 
->>>>>>> 378e16a0b444a7e48c9bbc31e96a56d69f34973f:vehicle.java
 public class vehicle {
     private String make; // for the make of the car
     private String model; // for the model of the car
-    private int year; // We can realistically pass the year as an int, date formating isn't necessary
+    private int year; // We can realistically pass the year as an int; date formatting isn't necessary
     private String plate; // License plate number
     private boolean parked; // Parked True/False
     private int assignedToJobID;
-<<<<<<< HEAD:classes/vehicle.java
-    //tranfered from the Ben branch worked on by Altin and Ben
     private int parkedTime;
     private int leftLotTime;
 
-
-    public vehicle(String make, String model, int year, String plate, boolean parked,
-    int parkedTime) {
-=======
-    private int parkedTime;
-    private int leftLotTime;
-
-    public vehicle(String make, String model, int year, String plate, boolean parked,
-            int parkedTime) {
->>>>>>> 378e16a0b444a7e48c9bbc31e96a56d69f34973f:vehicle.java
+    // Transferred from the Ben branch worked on by Altin and Ben
+    public vehicle(String make, String model, int year, String plate, boolean parked, int parkedTime) {
         this.make = make;
         this.model = model;
         this.year = year;
@@ -81,18 +63,14 @@ public class vehicle {
         return parked;
     }
 
-    public void setAssignedToJobID(int JobID) {
-        this.assignedToJobID = JobID;
+    public void setAssignedToJobID(int jobID) {
+        this.assignedToJobID = jobID;
     }
 
     public int getAssignedToJobID() {
         return assignedToJobID;
     }
-<<<<<<< HEAD:classes/vehicle.java
-    //From Ben Branch as well
-=======
 
->>>>>>> 378e16a0b444a7e48c9bbc31e96a56d69f34973f:vehicle.java
     public void setParkedTime(int parkedTime) {
         this.parkedTime = parkedTime;
     }
